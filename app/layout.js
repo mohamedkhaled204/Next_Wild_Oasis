b946e2dd096d70560b62695dc10f45ad11d0bb31
Mohamed Khaled <mohamedkhaledtharwat123@gmail.com>
2025-04-27 18:54:47 +0300

    g

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import Logo from "./_components/Logo"
 import '@/app/_style/globals.css'
-import Navigation from "./_components/Navigation"
 import {Josefin_Sans} from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets : ['latin'],
@@ -20,14 +20,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>
-          {children}
-        </main>
+      <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">
+            {children}
+          </main>
+        </div>
         </body>
     </html>
   )
