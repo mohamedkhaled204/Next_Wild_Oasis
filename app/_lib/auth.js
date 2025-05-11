86eefb338340bb6e2ce89f30176c9ab6cf6816b8
mohamedkhaled204 <mohamedkhaledtharwat123@gmail.com>
2025-05-11 23:12:00 +0300

    g

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -10,12 +11,28 @@ const authConfig = {
   ],
   callbacks: {
     authorized({ auth, request }) {
-      return !!auth?.user
-    }
+      return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullname: user.name });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
+    },
   },
   pages: {
-    signIn : "/login"
-  }
+    signIn: "/login",
+  },
 };
 
 export const {
